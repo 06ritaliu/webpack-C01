504022da312f200ca94711a573eee10d7e346b58
06ritaliu <liuting_jndx@163.com>
2025-03-13 19:07:42 +0800

    webpack的基础配置、基于spritesmith插件的本地生成精灵图脚本

---
+++ src/add-content.js
@@ -0,0 +1,8 @@
+/*
+ * @LastEditTime: 2025-03-09 12:37:04
+ * @Description: 
+ */
+import React from 'react';
+export default function() {
+    document.querySelector('#root').innerHTML = 'I am adding content to the page.' + React.version;
+}
